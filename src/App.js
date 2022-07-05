b18301c7db22fca46c714f2e0941b15da649304f
Daniel Bark <danba340@gmail.com>
2022-07-05 22:38:13 +0200

    -m

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ export default function App() {
       <div className="title">
         <h1>3D Nearby Plane Tracker</h1>
       </div>
+      <div className="canvas-placeholder">R3F Canvas placeholder</div>
       <div className="controls">
-        Controls panel
+        Control panel
       </div>
     </>
   )
